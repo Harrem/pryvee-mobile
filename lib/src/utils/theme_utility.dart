132b671abde409f893e25efe01f12b0c6bfc0ef8
Harrem <52822616+Harrem@users.noreply.github.com>
2022-11-13 14:34:47 +0300

    theme correction

--- lib/src/utils/theme_utility.dart
+++ lib/src/utils/theme_utility.dart
@@ -29,19 +29,45 @@ final darkTheme = ThemeData(
     focusColor: appConfig.Colors().accentDarkColor(0.8),
     textTheme: TextTheme(
         button: TextStyle(color: Colors.white),
-        headline2: TextStyle(fontSize: 22.0, fontWeight: FontWeight.w700, color: appConfig.Colors().mainDarkColor(1)),
-        headline1: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w400, color: appConfig.Colors().secondDarkColor(1)),
-        headline3: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w400, color: appConfig.Colors().secondDarkColor(1)),
-        headline4: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w400, color: appConfig.Colors().secondDarkColor(1)),
-        headline6: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w400, color: appConfig.Colors().mainDarkColor(1)),
-        bodyText2: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w400, color: appConfig.Colors().secondDarkColor(1)),
-        bodyText1: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w500, color: appConfig.Colors().secondDarkColor(1)),
-        caption: TextStyle(fontSize: 10.0, fontWeight: FontWeight.w500, color: appConfig.Colors().secondDarkColor(0.5))));
+        headline2: TextStyle(
+            fontSize: 22.0,
+            fontWeight: FontWeight.w700,
+            color: appConfig.Colors().mainDarkColor(1)),
+        headline1: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.w400,
+            color: appConfig.Colors().secondDarkColor(1)),
+        headline3: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.w400,
+            color: appConfig.Colors().secondDarkColor(1)),
+        headline4: TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.w400,
+            color: appConfig.Colors().secondDarkColor(1)),
+        headline6: TextStyle(
+            fontSize: 14.0,
+            fontWeight: FontWeight.w400,
+            color: appConfig.Colors().mainDarkColor(1)),
+        bodyText2: TextStyle(
+            fontSize: 12.0,
+            fontWeight: FontWeight.w400,
+            color: appConfig.Colors().secondDarkColor(1)),
+        bodyText1: TextStyle(
+            fontSize: 12.0,
+            fontWeight: FontWeight.w500,
+            color: appConfig.Colors().secondDarkColor(1)),
+        caption: TextStyle(
+            fontSize: 10.0,
+            fontWeight: FontWeight.w500,
+            color: appConfig.Colors().secondDarkColor(0.5))));
 final lightTheme = ThemeData(
     fontFamily: 'Nunito',
     primaryColor: Colors.white,
     appBarTheme: AppBarTheme(
-      color: Colors.transparent,
+      foregroundColor: Colors.black,
+      color: Colors.white,
+      elevation: 0,
       systemOverlayStyle: SystemUiOverlayStyle.dark,
     ),
     brightness: Brightness.light,
@@ -63,11 +89,35 @@ final lightTheme = ThemeData(
     focusColor: appConfig.Colors().accentColor(0.8),
     textTheme: TextTheme(
         button: TextStyle(color: Colors.white),
-        headline2: TextStyle(fontSize: 22.0, fontWeight: FontWeight.w700, color: appConfig.Colors().mainColor(1)),
-        headline1: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w400, color: appConfig.Colors().secondColor(1)),
-        headline3: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w400, color: appConfig.Colors().secondColor(1)),
-        headline4: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w400, color: appConfig.Colors().secondColor(1)),
-        headline6: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w400, color: appConfig.Colors().mainColor(1)),
-        bodyText2: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w400, color: appConfig.Colors().secondColor(1)),
-        bodyText1: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w500, color: appConfig.Colors().secondColor(1)),
-        caption: TextStyle(fontSize: 10.0, fontWeight: FontWeight.w500, color: appConfig.Colors().secondColor(0.5))));
+        headline2: TextStyle(
+            fontSize: 22.0,
+            fontWeight: FontWeight.w700,
+            color: appConfig.Colors().mainColor(1)),
+        headline1: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.w400,
+            color: appConfig.Colors().secondColor(1)),
+        headline3: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.w400,
+            color: appConfig.Colors().secondColor(1)),
+        headline4: TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.w400,
+            color: appConfig.Colors().secondColor(1)),
+        headline6: TextStyle(
+            fontSize: 14.0,
+            fontWeight: FontWeight.w400,
+            color: appConfig.Colors().mainColor(1)),
+        bodyText2: TextStyle(
+            fontSize: 12.0,
+            fontWeight: FontWeight.w400,
+            color: appConfig.Colors().secondColor(1)),
+        bodyText1: TextStyle(
+            fontSize: 12.0,
+            fontWeight: FontWeight.w500,
+            color: appConfig.Colors().secondColor(1)),
+        caption: TextStyle(
+            fontSize: 10.0,
+            fontWeight: FontWeight.w500,
+            color: appConfig.Colors().secondColor(0.5))));
