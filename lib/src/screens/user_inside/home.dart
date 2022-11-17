0dcb8a82121bcc0543b572ed79645d2b2e2efb5a
Harrem <52822616+Harrem@users.noreply.github.com>
2022-11-17 21:39:56 +0300

    live posts from real data

--- lib/src/screens/user_inside/home.dart
+++ lib/src/screens/user_inside/home.dart
@@ -1,3 +1,8 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:provider/provider.dart';
+import 'package:pryvee/src/models/post.dart';
+import 'package:pryvee/src/providers_utils/post_provider.dart';
+import 'package:pryvee/src/providers_utils/user_data_provider.dart';
 import 'package:pryvee/src/widgets/shared_inside/CustomSearchBarWidget.dart';
 import 'package:pryvee/src/widgets/LivePostItemWidget.dart';
 import 'package:pryvee/data/data_source_const.dart';
@@ -41,6 +46,7 @@ class _HomeWidgetState extends State<HomeWidget> {
 
   @override
   Widget build(BuildContext context) {
+    final userProvider = Provider.of<UserProvider>(context);
     return ListView(
       padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
       children: [
@@ -78,19 +84,10 @@ class _HomeWidgetState extends State<HomeWidget> {
           ],
         ),
         SizedBox(height: 12.0),
-        CustomSearchBarWidget(
-          searchTextEditingController: this.searchTextEditingController,
-        ),
+        // CustomSearchBarWidget(
+        //   searchTextEditingController: this.searchTextEditingController,
+        // ),
         SizedBox(height: 12.0),
-        Column(
-          children: [
-            ElevatedButton(
-              onPressed: () => Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => AddNewPosWidget())),
-              child: Text("Going out?"),
-            )
-          ],
-        ),
         SizedBox(height: 12.0),
         Row(
           children: [
@@ -117,17 +114,60 @@ class _HomeWidgetState extends State<HomeWidget> {
           ],
         ),
         SizedBox(height: 12.0),
-        ListView.separated(
-          physics: NeverScrollableScrollPhysics(),
-          separatorBuilder: (context, index) => SizedBox(height: 8.0),
-          padding: EdgeInsets.zero,
-          shrinkWrap: true,
-          itemCount: 1,
-          itemBuilder: (context, index) {
-            return LivePostItemWidget();
+        FutureBuilder<List<Post>>(
+          future: PostOperations.fetchAllPostes(userProvider.uid),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: const CircularProgressIndicator(),
+              );
+            }
+            if (snapshot.hasData && snapshot.data.isNotEmpty) {
+              List<Post> posts = [];
+              snapshot.data.forEach((post) {
+                if (post.isLive) {
+                  posts.add(post);
+                }
+              });
+              return ListView.separated(
+                physics: NeverScrollableScrollPhysics(),
+                separatorBuilder: (context, index) => SizedBox(height: 8.0),
+                padding: EdgeInsets.zero,
+                shrinkWrap: true,
+                itemCount: posts.length,
+                itemBuilder: (context, index) {
+                  return LivePostItemWidget(
+                    post: posts[index],
+                    userData: userProvider.userData,
+                  );
+                },
+              );
+            } else {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    "you don't have any posts yet!",
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                  ElevatedButton(
+                    onPressed: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => AddNewPosWidget())),
+                    child: Text("Going out?"),
+                  ),
+                ],
+              );
+            }
           },
         ),
         SizedBox(height: 12.0),
+        ElevatedButton(
+            onPressed: () {
+              AwesomeNotifications().cancelAll();
+            },
+            child: Text("Cancel Notifications"))
       ],
     );
   }
