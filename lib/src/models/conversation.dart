3dea8db1c15f7a603b95def1c1f84f52e309ac23
Harrem <52822616+Harrem@users.noreply.github.com>
2022-12-09 23:39:16 +0300

    implemented conversation functionality

--- lib/src/models/conversation.dart
+++ lib/src/models/conversation.dart
@@ -1,33 +1,41 @@
 import 'dart:convert';
 
+import 'package:pryvee/src/models/message.dart';
+
 class Conversation {
   String cid;
   DateTime createdDate;
+  DateTime updatedDate;
   String docRef;
   String user1;
   String user2;
+  Message lastMessage;
 
-  Conversation({
-    this.cid,
-    this.createdDate,
-    this.docRef,
-    this.user1,
-    this.user2,
-  });
+  Conversation(
+      {this.cid,
+      this.createdDate,
+      this.updatedDate,
+      this.docRef,
+      this.user1,
+      this.user2,
+      this.lastMessage});
 
-  Conversation copyWith({
-    String cid,
-    DateTime createdDate,
-    String docRef,
-    String user1,
-    String user2,
-  }) {
+  Conversation copyWith(
+      {String cid,
+      DateTime createdDate,
+      DateTime updatedDate,
+      String docRef,
+      String user1,
+      String user2,
+      Message lastMessage}) {
     return Conversation(
       cid: cid ?? this.cid,
       createdDate: createdDate ?? this.createdDate,
+      updatedDate: updatedDate ?? this.updatedDate,
       docRef: docRef ?? this.docRef,
       user1: user1 ?? this.user1,
       user2: user2 ?? this.user2,
+      lastMessage: lastMessage ?? this.lastMessage,
     );
   }
 
@@ -35,21 +43,28 @@ class Conversation {
     return <String, dynamic>{
       'cid': cid,
       'createdDate': createdDate.millisecondsSinceEpoch,
+      'updatedDate': updatedDate.millisecondsSinceEpoch,
       'docRef': docRef,
       'user1': user1,
       'user2': user2,
+      'lastMessage': lastMessage,
     };
   }
 
   factory Conversation.fromMap(Map<String, dynamic> map) {
     return Conversation(
-      cid: map['cid'] as String,
-      createdDate:
-          DateTime.fromMillisecondsSinceEpoch(map['createdDate'] as int),
-      docRef: map['docRef'] != null ? map['docRef'] as String : null,
-      user1: map['user1'] as String,
-      user2: map['user2'] as String,
-    );
+        cid: map['cid'] as String,
+        createdDate:
+            DateTime.fromMillisecondsSinceEpoch(map['createdDate'] as int),
+        updatedDate: (map['updatedDate'] == null)
+            ? DateTime.fromMillisecondsSinceEpoch(map['createdDate'] as int)
+            : DateTime.fromMillisecondsSinceEpoch(map['updatedDate'] as int),
+        docRef: map['docRef'] != null ? map['docRef'] as String : null,
+        user1: map['user1'] as String,
+        user2: map['user2'] as String,
+        lastMessage: (map['lastMessage'] == null)
+            ? null
+            : Message.fromMap((map['lastMessage'] as Map<String, dynamic>)));
   }
 
   String toJson() => json.encode(toMap());
@@ -59,7 +74,7 @@ class Conversation {
 
   @override
   String toString() {
-    return 'Conversation(cid: $cid, createdDate: $createdDate, docRef: $docRef, user1: $user1, user2: $user2)';
+    return 'Conversation(cid: $cid, createdDate: $createdDate, updatedDate: $updatedDate, docRef: $docRef, user1: $user1, user2: $user2, lastMessage: $lastMessage)';
   }
 
   @override
@@ -68,17 +83,21 @@ class Conversation {
 
     return other.cid == cid &&
         other.createdDate == createdDate &&
+        other.updatedDate == updatedDate &&
         other.docRef == docRef &&
         other.user1 == user1 &&
-        other.user2 == user2;
+        other.user2 == user2 &&
+        other.lastMessage == lastMessage;
   }
 
   @override
   int get hashCode {
     return cid.hashCode ^
         createdDate.hashCode ^
+        updatedDate.hashCode ^
         docRef.hashCode ^
         user1.hashCode ^
-        user2.hashCode;
+        user2.hashCode ^
+        lastMessage.hashCode;
   }
 }
